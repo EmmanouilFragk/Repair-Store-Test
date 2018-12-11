1ce308c70c136dfca46b7870fafb2f7cc41391e1
kyriakos133 <karageo133@gmail.com>
2018-12-11 16:05:24 +0200

    DataRepair

---
+++ src/main/java/com/example/demo/service/RepairServiceImpl.java
@@ -0,0 +1,19 @@
+package com.example.demo.service;
+import com.example.demo.domain.Repair;
+import com.example.demo.repository.RepairRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import java.util.List;
+
+@Component
+public class RepairServiceImpl implements RepairService {
+
+    @Autowired
+    private RepairRepository repairRepository;
+
+    @Override
+    public List<Repair> findAll() {
+        return repairRepository.findAll();
+    }
+
+}
